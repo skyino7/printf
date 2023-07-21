3feb7490e873cfa5c757568586e37644159d1579
AbduGad <abdo79gad@gmail.com>
2023-07-21 18:21:02 +0300

    main.c test2

--- main.c
+++ main.c
@@ -11,11 +11,13 @@ int main(void)
 {
     int len;
     int len2;
-    
 
     len = _printf("Let's try to printf a simple sentence.\n");
     len2 = printf("Let's try to printf a simple sentence.\n");
-    
+
+    printf("%d\n", len);
+    printf("%d\n", len2);
+
     _printf("Character:[%c]\n", 'H');
     printf("Character:[%c]\n", 'H');
     _printf("String:[%s]\n", "I am a string !");
