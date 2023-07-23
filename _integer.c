d8e43eaaaaa2a3cdbb8ee12b1d03189f938d73b2
Skyino7 <christambassador12@gmail.com>
2023-07-23 01:49:57 +0000

    Integer Function Test

--- _integer.c
+++ _integer.c
@@ -6,7 +6,7 @@
  * @pcount: pointer
  */
 
-void _integer(int num, int *pcount)
+/* void _integer(int num, int *pcount)
 {
     int Tens = 1;
    int cpy;
@@ -36,4 +36,38 @@ void _integer(int num, int *pcount)
    _putchar((num % 10) + '0');
    *pcount += 1;
 
+} */
+
+int _integer(int num)
+{
+    int count = 0, tmp, digit = 0;
+
+    if (num < 0)
+    {
+        _putchar('-');
+        count++;
+        num = -num;
+    }
+
+    if (num == 0)
+    {
+        _putchar(48);
+        return 1;
+    }
+
+    tmp = num;
+    while (tmp > 0)
+    {
+        count++;
+        tmp /= 10;
+    }
+
+    while (num > 0)
+    {
+        digit = num % 10;
+        _putchar(digit + 48);
+        num /= 10;
+    }
+    
+    return (count);
 }
--- main.h
+++ main.h
@@ -9,6 +9,7 @@ int _putchar(char c);
 void _string(char *s, int *pcount);
 void _char(char c, int *pcount);
 int _printf(const char *format, ...);
-void _integer(int num, int *pcount);
+/* void _integer(int num, int *pcount); */
+int _integer(int num);
 
 #endif /*MAIN_H*/
