a5404c53741eb772896eca20b6659c63202e16ba
Skyino7 <christambassador12@gmail.com>
2023-07-23 00:25:20 +0000

    Testing the integer function

--- _integer.c
+++ _integer.c
@@ -17,22 +17,40 @@ void _integer(int num, int *pcount)
      *pcount += 1;
      num *= -1;
    }
+
+   if (num == 0)
+   {
+    _putchar('0');
+    *pcount += 1;
+   }
+
+   if (num >= 0 && num <= 9)
+   {
+    _putchar(num + '0');
+    *pcount += 1;
+   }
+   
+   
    cpy = num;
    
-   while (cpy > 9)
+   if (num > 9)
    {
-       Tens *= 10;
-       cpy = cpy / 10;
-   }
    
-   while (num > 9)
-   {
-       _putchar(num / Tens + '0');
-       *pcount += 1;
-       num = num % Tens;
-       Tens /= 10;
+        while (cpy > 9)
+        {
+            Tens *= 10;
+            cpy = cpy / 10;
+        }
+        
+        while (num > 9)
+        {
+            _putchar(num / Tens + '0');
+            *pcount += 1;
+            num = num % Tens;
+            Tens /= 10;
+        }
+        _putchar((num % 10) + '0');
+        *pcount += 1;
    }
-   _putchar((num % 10) + '0');
-   *pcount += 1;
 
 }
