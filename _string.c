0fd4b278d65c74218d786416f5d52e5e21173a1f
Skyino7 <christambassador12@gmail.com>
2023-07-23 17:55:22 +0000

    integer errors corrected.

--- _string.c
+++ _string.c
@@ -5,15 +5,16 @@
  * @format: string input
  * @pcount: character counter for the printf return
  * @arg: va_list
+ * Return: 1
  */
 
 int _string(const char *format, va_list arg, int *pcount)
-{	
+{
 	int j;
 	char *s = va_arg(arg, char *);
 
 	(void)format;
-	if(!s)
+	if (!s)
 		s = "(null)";
     /*char *s = va_arg(ars, char*);*/
 	for (j = 0; s[j] != '\0'; j++)
@@ -21,5 +22,5 @@ int _string(const char *format, va_list arg, int *pcount)
 		_putchar(s[j]);
 		*pcount += 1;
 	}
-	return(1);
+	return (1);
 }
