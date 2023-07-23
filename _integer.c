83741eb87e21f22dc45acf6f0eb3407bada04433
Skyino7 <christambassador12@gmail.com>
2023-07-23 18:04:14 +0000

    Betty Style Errors Corrected

--- _integer.c
+++ _integer.c
@@ -5,39 +5,40 @@
  * @format: string input
  * @pcount: character counter for the printf return
  * @arg: va_list
+ * Return: 1
  */
 
 int _integer(const char *format, va_list arg, int *pcount)
 {
-    int Tens = 1;
-    int cpy;
-    int num = va_arg(arg, int);
+	int Tens = 1;
+	int cpy;
+	int num = va_arg(arg, int);
 
-    (void)format;
-    if (num < 0)
-    {
-        _putchar('-');
-        *pcount += 1;
-        num *= -1;
-    }
-    
-    cpy = num;
-    
-    while (cpy > 9)
-    {
-        Tens *= 10;
-        cpy = cpy / 10;
-    }
-    
-    while (num > 9)
-    {
-        _putchar(num / Tens + '0');
-        *pcount += 1;
-        num = num % Tens;
-        Tens /= 10;
-    }
-    _putchar((num % 10) + '0');
-    *pcount += 1;
+	(void)format;
+	if (num < 0)
+	{
+		_putchar('-');
+		*pcount += 1;
+		num *= -1;
+	}
 
-    return (1);
+	cpy = num;
+
+	while (cpy > 9)
+	{
+		Tens *= 10;
+		cpy = cpy / 10;
+	}
+
+	while (num > 9)
+	{
+		_putchar(num / Tens + '0');
+		*pcount += 1;
+		num = num % Tens;
+		Tens /= 10;
+	}
+	_putchar((num % 10) + '0');
+	*pcount += 1;
+
+	return (1);
 }
