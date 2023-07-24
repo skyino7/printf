f4c7befe4b482401c517abfa4a5fce10449655b6
AbduGad <abdo79gad@gmail.com>
2023-07-24 13:26:03 +0300

    test

--- printf.c
+++ printf.c
@@ -22,7 +22,7 @@ int _printf(const char *format, ...)
 			return (-1);
 		else if (format[i] == '%')
 		{
-			if (format[i + j] == '\0' || format[i + j] == ' ')
+			if (format[i + j] == '\0')
 			{
 				return (-1);
 			}
