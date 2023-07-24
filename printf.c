40f8d7f97a6d8c2567af6bf6797dfe1cb715d2f0
Skyino7 <christambassador12@gmail.com>
2023-07-24 08:58:58 +0000

    Test

--- printf.c
+++ printf.c
@@ -12,8 +12,8 @@ int _printf(const char *format, ...)
 	sp output[5] = {{'c', _char}, {'s', _string}, {'%', _module},
 		{'i', _integer}, {'d', _integer}};
 
-	/* if (!format)
-		return (-1); */
+	if (!format)
+		return (-1);
 	va_start(args, format);
 	(void)flag;
 	for (i = 0; format[i] != '\0';)
