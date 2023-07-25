c10457594e028221d4804eee54a64d6d75849715
AbduGad <abdo79gad@gmail.com>
2023-07-25 16:38:24 +0300

    binary test

---
+++ _binary.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _binary -  unsigned int argument is converted to binary
+ * 
+ * @fomrat: input
+ * @args: va_list input
+ * @pcount: counter pointer
+ * Return: flag
+ */
+
+int _binary(const char *format, va_list args, int *pcount)
+{
+    unsigned int num = va_arg(args, unsigned int);
+    int Tens = 1, result = 0;
+
+    (void)format;
+    while (num)
+    {
+        result += (num % 2) * Tens;
+        Tens *= 10;
+        num /= 2;
+    }
+    while (result > 9 || Tens > 1)
+	{
+		_putchar(num / Tens + '0');
+		*pcount += 1;
+		num = num % Tens;
+		Tens /= 10;
+	}
+	_putchar((num % 10) + '0');
+	*pcount += 1;
+    return (1);
+}
