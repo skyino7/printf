2cfced7253bdc21a3385925101444295088e02f4
AbduGad <abdo79gad@gmail.com>
2023-07-26 12:52:21 +0300

    test b

--- _binary.c
+++ _binary.c
@@ -12,25 +12,25 @@
 int _b(const char *format, va_list arg, int *pcount)
 {
 	unsigned int num = va_arg(arg, unsigned int);
-	unsigned long int Tens = 1;
-	unsigned int result = 0;
+	int length = 0;
+	char buffer[33];
 
 	(void)format;
+	if (num == 0)
+	{
+		_putchar('0');
+		*pcount += 1;
+	}
 	while (num)
 	{
-		result += (num % 2) * Tens;
-		Tens *= 10;
+		buffer[length] = (num % 2) + '0';
 		num /= 2;
+		length++;
 	}
-	Tens /= 10;
-	while (result > 9 || Tens > 1)
+	while (length > 0)
 	{
-		_putchar(result / Tens + '0');
+		_putchar(buffer[--length]);
 		*pcount += 1;
-		result = result % Tens;
-		Tens /= 10;
 	}
-	_putchar((result % 10) + '0');
-	*pcount += 1;
 	return (1);
 }
